d0f939a8e2e63844d81793ff0b55c899acbdbca6
snjezamijosevic <snjezana.mijosevic@gmail.com>
2016-07-21 17:51:55 +0200

    Update prijava.php

--- prijava.php
+++ prijava.php
@@ -5,7 +5,7 @@
 			  src="https://code.jquery.com/jquery-3.1.0.min.js"
 			  integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
 			  crossorigin="anonymous"></script>
-<link rel="stylesheet" type="text/css" href="css/stilprijave.css">
+<link rel="stylesheet" type="text/css" href="stilprijave.css">
 </head>
 <body>
 <form method="post" action="probaj.php" id="prva">
